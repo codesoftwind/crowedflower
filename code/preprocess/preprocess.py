059794513402bd72d0776d5db31e899fae9b5c85
Ryan Wang <wangdong@RyandeMacBook-Pro.local>
2017-09-19 13:10:30 +0800

    完成了去除html标签和词型还原的工作，拼写改正和同义词替换暂时未做

--- code/preprocess/preprocess.py
+++ code/preprocess/preprocess.py
@@ -1,59 +1,68 @@
 #-*-coding:utf-8-*-
 
+"""
+预处理
+1.去除html标签
+2.拼写改正
+3.词性还原
+4.同义词替换
+5.去除停用词
+"""
+
+
 import pandas as pd
 from bs4 import BeautifulSoup
 from textblob import *
+import nltk
 from nltk.corpus import wordnet
 from nltk import word_tokenize, pos_tag
 from nltk.stem import WordNetLemmatizer
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def read_csv(path):
     return pd.read_csv(path)
+
 
 def drophtmltags(df):
     df['product_description'] = df['product_description'].apply(lambda x: BeautifulSoup(str(x), 'html5lib').get_text(separator=" "))
     return df
 
-def spellcorrect(df):
+def stemming(stemmer, sentence):
+    stemmed = []
+    wordList = word_tokenize(str(sentence).decode('utf-8'))
+    for word in wordList:
+        stemmedWord = stemmer.stem(word)
+        stemmed.append(stemmedWord)
+        if word != stemmedWord:
+            print word, '---', stemmedWord
+    return ' '.join(stemmed)
+
+def stemDF(df):
     columns = ['query', 'product_title', 'product_description']
+    stemmer = nltk.stem.SnowballStemmer('english')
     for column in columns:
-        df[column] = df[column].apply(lambda x: str(TextBlob(x).correct()))
+        df[column] = df[column].apply(lambda x: stemming(stemmer, x))
     return df
 
-def get_wordnet_pos(treebank_tag):
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return None
-
-#word_tokenize将sentence分词，pos_tag识别词性，返回一个元组的list(word,pos)
-def lemmatize_sentence(sentence):
-    res = []
-    lemmatizer = WordNetLemmatizer()
-    for word, pos in pos_tag(word_tokenize(sentence)):
-        wordnet_pos = get_wordnet_pos(pos) or wordnet.NOUN
-        res.append(lemmatizer.lemmatize(word, pos=wordnet_pos))
-    return ' '.join(res)
-
-
-def lemmatize(csv):
-    columns = ['query', 'product_title', 'product_description']
-    for column in columns:
-        csv[column] = csv[column].apply(lambda x: lemmatize_sentence(str(x).decode('utf-8')))
-    return csv
-
+# def spellcorrect(df):
+#     columns = ['query', 'product_title', 'product_description']
+#     for column in columns:
+#         df[column] = df[column].apply(lambda x: str(TextBlob(str(x).decode('utf-8')).correct()))
+#     return df
 
 if __name__ == '__main__':
-    df = read_csv('/Users/wangdong/repos/crowedflower/data/train_copy.csv').fillna("")
-    df[['id', 'median_relevance', 'relevance_variance']] = df[['id', 'median_relevance', 'relevance_variance']].apply(pd.to_numeric)
+    (originDataPath, processedDataPath) = (sys.argv[1], sys.argv[2])
+    print time.asctime(time.localtime(time.time()))
+    df = read_csv(originDataPath).fillna("")
+    #df[['id', 'median_relevance', 'relevance_variance']] = df[['id', 'median_relevance', 'relevance_variance']].apply(pd.to_numeric)
     df = drophtmltags(df)
-    df = spellcorrect(df)
-    df = lemmatize(df)
-    df.to_csv('newtarin.csv', encoding='utf-8')
-    print df
+    print '-------------------------------------'
+    print 'drop html tags finished!'
+    df = stemDF(df)
+    print '-------------------------------------'
+    print 'stemming finished!'
+    df.to_csv(processedDataPath, encoding='utf-8')
+    print time.asctime(time.localtime(time.time()))
